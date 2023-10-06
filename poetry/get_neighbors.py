33decfb64a1e106b8da4214575b94257c98a5900
Laura Fuentes <laura.fuentes@newstreetresearch.com>
2023-10-06 07:46:53 -0500

    poetry project

---
+++ poetry/get_neighbors.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+train = np.loadtxt('data/synth_train.txt')  
+class_train = train[:,0]
+x_train = train[:,1:]
+N_train = train.shape[0]
+
+# load the test set
+test = np.loadtxt('data/synth_test.txt') 
+class_test_1 = test[test[:,0]==1]
+class_test_2 = test[test[:,0]==2]
+x_test = test[:,1:]
+N_test = test.shape[0]
+
+# Locate the most similar neighbors
+def get_neighbors(train, test_row, num_neighbors):
+	distances = list()
+	for train_row in train:
+		dist = np.linalg.norm(test_row-train_row)
+		distances.append((train_row, dist))
+		
+	distances.sort(key=lambda tup: tup[1])
+	neighbors = list()
+	for i in range(num_neighbors):
+		neighbors.append(distances[i][0])
+	return neighbors
+
+
+#neighbors = get_neighbors(x_train, x_train[0], 3)
+
