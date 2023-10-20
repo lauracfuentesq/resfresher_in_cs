6a263e1201087c7cedea4e5a32e6a1b4c26fc89f
Laura Fuentes <laura.fuentes@newstreetresearch.com>
2023-10-20 01:22:22 -0500

    read data only in test file

--- rp_poetry/get_neighbors.py
+++ rp_poetry/get_neighbors.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 
-train = np.loadtxt('data\synth_train.txt')  
-class_train = train[:,0]
-x_train = train[:,1:]
-N_train = train.shape[0]
+# train = np.loadtxt('data\synth_train.txt')  
+# class_train = train[:,0]
+# x_train = train[:,1:]
+# N_train = train.shape[0]
 
-# load the test set
-test = np.loadtxt('data\synth_test.txt') 
-class_test_1 = test[test[:,0]==1]
-class_test_2 = test[test[:,0]==2]
-x_test = test[:,1:]
-N_test = test.shape[0]
+# # load the test set
+# test = np.loadtxt('data\synth_test.txt') 
+# class_test_1 = test[test[:,0]==1]
+# class_test_2 = test[test[:,0]==2]
+# x_test = test[:,1:]
+# N_test = test.shape[0]
 
 # Locate the most similar neighbors
 def get_neighbors(train, test_row, num_neighbors):
